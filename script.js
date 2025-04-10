7b081e9efbd547f315ca411dc90a47d2a56bb446
ManyManicAmbitions <russelltyrel9@gmail.com>
2025-04-10 12:59:03 -0400

    Added basic html structure and js function(student info) for part 1

---
+++ script.js
@@ -0,0 +1,17 @@
+function student(name, age, enrolled, courses){
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.courses = courses;
+
+        this.displayInfo = function(){
+            console.log(`Name: ${this.name}, Age: ${this.age}, Enrolled: ${this.enrolled}, Courses: ${this.courses} `);
+            
+        };
+}
+
+    const student1 = new student("Tyrel", 24, true, ["CPAN-111-RNA", "CPAN-133-RNA", "CPAN-116-RNA", "WRIT-120-DWB", "CPAN-113-RNA", "CPAN-134-RNA"]);
+        console.log(student1.name, student1.age);
+
+        student1.displayInfo();
+        
